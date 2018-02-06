d2e2b9980b2dd8baa439a66c1cc62c8f6d610e0b
王靖凯 <wjk930726@live.com>
2018-02-06 23:18:20 +0800

    grmbl

--- backupZeroNet.sh
+++ backupZeroNet.sh
@@ -8,7 +8,7 @@ users_json="./users.json"
 sites_json="./sites.json"
 mutes_json="./mutes.json"
 
-cloud_backup_dir="/Users/wangjingkai/Library/Mobile\ Documents/com~apple~CloudDocs/ZoreNet"
+cloud_backup_dir="/Users/wangjingkai/Library/Mobile Documents/com~apple~CloudDocs/ZoreNet"
 local_backup_dir="/Users/wangjingkai/资源/归档/ZoreNet"
 
 ###########MAIN##################
@@ -23,12 +23,18 @@ fi
 # tar and gzip
 cd "${baseDir}"
 tar -zcvf "${cloud_backup_dir}/zeroNetBackup+${now}.tar.gz" "${users_json}" "${sites_json}" "${mutes_json}"
-# tar -zcvf "${local_backup_dir}/zeroNetBackup+${now}.tar.gz" "${users_json}" "${sites_json}" "${mutes_json}"
+tar -zcvf "${local_backup_dir}/zeroNetBackup+${now}.tar.gz" "${users_json}" "${sites_json}" "${mutes_json}"
 
 num=`ls -l "${cloud_backup_dir}" |grep "^-"|wc -l`
 if [ ${num} -gt 5 ]; then
     num=`expr ${num} - 5`
-    ls -tr "${cloud_backup_dir}" | head -${num} | xargs
+    cd "${cloud_backup_dir}"
+    ls -tr "${cloud_backup_dir}" | head -${num} | xargs rm
 fi
 
-echo `ls -l "${cloud_backup_dir}" |grep "^-"|wc -l`
+num=`ls -l "${local_backup_dir}" |grep "^-"|wc -l`
+if [ ${num} -gt 5 ]; then
+    num=`expr ${num} - 5`
+    cd "${local_backup_dir}"
+    ls -tr "${local_backup_dir}" | head -${num} | xargs rm
+fi
