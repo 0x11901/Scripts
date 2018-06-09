fbc60925d3e54b0af491f34c9d95982cc767eaab
王靖凯 <wjk930726@live.com>
2018-06-09 23:53:11 +0800

    Major fixup.

--- upgradeCask.sh
+++ upgradeCask.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+help=`brew cu --help | head -1`
+if [ "brew cu [options]" = "${help}" ]; then
+    brew cu -fa --yes
+else
 (set -x; brew update;)
 
 (set -x; brew cleanup;)
@@ -14,8 +18,8 @@ casks=( $(brew cask list) )
 for cask in ${casks[@]}
 do
     version=$(brew cask info ${cask} | sed -n "s/$cask:\ \(.*\)/\1/p")
-    installed=$(find "/usr/local/Caskroom/$cask" -type d -maxdepth 1 -maxdepth 1 -name "$version")
-    
+    installed=$(find "/usr/local/Caskroom/$cask" -type d -maxdepth 1 -name "$version")
+
     if [[ -z ${installed} ]]; then
         echo "${red}${cask}${reset} requires ${red}update${reset}."
         (set -x; brew cask uninstall ${cask} --force;)
@@ -24,3 +28,4 @@ do
         echo "${red}${cask}${reset} is ${green}up-to-date${reset}."
     fi
 done
+fi
